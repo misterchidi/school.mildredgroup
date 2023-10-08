a3357ba0722ee487053e54356699ba48956dade7
misterchidi <chillazone@yahoo.com>
2023-10-08 08:55:51 -0700

    First commit

---
+++ assignment_4/name.py
@@ -0,0 +1,5 @@
+first_name = "Chidi "
+middle_name = "Leonard "
+last_name = "Nwachukwu"
+
+print (first_name + middle_name + last_name)
---
+++ assignment_4/variable_types.py
@@ -0,0 +1,26 @@
+a = 5
+b = 2.5
+c = "Hello"
+d = True
+e = [1, 2, 3]
+f = (4, 5, 6)
+g = {"name": "Ben", "age": 20}
+h = {1, 2, 3, 4}
+
+type_a = type(a)
+type_b = type(b)
+type_c = type(c)
+type_d = type(d)
+type_e = type(e)
+type_f = type(f)
+type_g = type(g)
+type_h = type(h)
+
+print(f"The value of a is {a} and the data type of a is {type_a}")
+print(f"The value of b is {b} and the data type of b is {type_b}")
+print(f"The value of c is {c} and the data type of c is {type_c}")
+print(f"The value of d is {d} and the data type of d is {type_d}")
+print(f"The value of e is {e} and the data type of e is {type_e}")
+print(f"The value of f is {f} and the data type of f is {type_f}")
+print(f"The value of g is {g} and the data type of g is {type_g}")
+print(f"The value of h is {h} and the data type of h is {type_h}")
